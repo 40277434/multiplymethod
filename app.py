254c89ee3249005c85aa1ab539f832dda206325d
Zhuofan Wei <40277434@eeecs.qub.ac.uk>
2021-03-23 20:39:32 +0000

    update commit

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, request
+
+
+
 
 app = Flask(__name__)
 
+
+
 def multiply(x,y):
-    return x*y;
+
+    return int(x)*int(y);
+
     
+
 @app.route('/')
+
 def hello_world():
+
     x = request.args.get('x')
+
     y = request.args.get('y')
-    return multiply(x,y)
+
+    result= dict()
+    result['answer']=multiply(x,y)
+    return result
+
     
+
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=80)
+
+    app.run(host='0.0.0.0')
+
       
+

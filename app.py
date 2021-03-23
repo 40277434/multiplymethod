e9e3856ef7d196923e3fe10255dbd8f36f36dd1b
Zhuofan Wei <40277434@ads.qub.ac.uk>
2021-03-23 20:18:18 +0000

    third commit

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    """Print 'Hello, world!' as the response body."""
-    return 'Hello, world!'
-    
+    x = request.args.get('x')
+    y = request.args.get('y')
+    return multiply(x,y)
     
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80)
+      
+def multiply(x,y):
+    return x*y;

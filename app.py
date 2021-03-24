2e959825a03e2cb66ab8b5a059b85714474210b2
40277434 <56730561+40277434@users.noreply.github.com>
2021-03-24 20:01:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,50 @@
-from flask import Flask, request
-
-
-
+from flask import Flask, request,redirect,url_for
 
 app = Flask(__name__)
 
 
-
-def multiply(x,y):
-
-    return int(x)*int(y);
+def multiply(x, y):
+    return int(x) * int(y);
 
 
+@app.route('/<x>/<y>')
+@app.route('/index/<x>/<y>')
+@app.route('/home/<x>/<y>')
+def hello_world(x,y):
+    #x = request.args.get('x')
+    #y = request.args.get('y')
+    if(x.isdigit() and y.isdigit()):
+        result = dict()
+        result['answer'] = multiply(x, y)
+        return result
+    else :
+        return demo2
 
-    
 
-@app.route('/')
+@app.route('/demo0')
+def demo0():
+    return 'Webpage not found'
 
-def hello_world():
+@app.route('/demo1')
+def demo1():
+    return 'Pay attention to input parameters'
 
-    x = request.args.get('x')
+@app.route('/demo2')
+def demo2():
+    return 'The input data type is not an integer'
 
-    y = request.args.get('y')
 
-    result= dict()
-    result['answer']=multiply(x,y)
-    return result
+@app.errorhandler(404)
+def demo0(e):
+    return redirect(url_for('demo0'))
+@app.errorhandler(500)
+def demo1(e):
+    return redirect(url_for('demo1'))
+@app.errorhandler(500)
+def demo2(e):
+    return redirect(url_for('demo2'))
 
-    
 
 if __name__ == '__main__':
-
-    app.run(host='0.0.0.0',port=80)
-
-      
-
+     app.run()
+    #app.run(host='0.0.0.0', port=80)
